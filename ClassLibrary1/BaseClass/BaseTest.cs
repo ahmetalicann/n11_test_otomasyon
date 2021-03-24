15d30751ac54a52c10ade4a9a6e739037ad5b0a6
ahmettaliicann@gmail.com <ahmettaliicann@gmail.com>
2021-03-24 17:00:58 +0300

    Add project files.

---
+++ ClassLibrary1/BaseClass/BaseTest.cs
@@ -0,0 +1,133 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Interactions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1.BaseClass
+{
+    public class BaseTest
+    {
+        public IWebDriver driver;
+
+        [SetUp]
+        public void Open()
+        {
+            driver = new ChromeDriver();
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+            driver.Manage().Window.Maximize();
+            driver.Url = "https://www.n11.com/";
+        }
+
+        [TearDown]
+        public void Close()
+        {
+            driver.Quit();
+        }
+
+        public void search()
+        {
+            driver.FindElement(By.Id("searchData")).SendKeys("Spor ayakkabı" + OpenQA.Selenium.Keys.Enter);
+        }
+
+        public void kategori(string x, string y)
+        {
+            Actions act = new Actions(driver);
+            var element = driver.FindElement(By.XPath(x));
+            act.MoveToElement(element).Perform();
+            driver.FindElement(By.XPath(y)).Click();
+        }
+        public void urlGit(string x)
+        {
+            driver.Navigate().GoToUrl(x);
+        }
+
+        public void Search()
+        {
+            driver.FindElement(By.Id("searchData")).SendKeys("Spor ayakkabı" + OpenQA.Selenium.Keys.Enter);
+        }
+
+        public void cssClick(string x)
+        {
+            driver.FindElement(By.CssSelector(x)).Click();
+            Thread.Sleep(1000);
+        }
+
+        public void xpathClick(string x)
+        {
+            driver.FindElement(By.XPath(x)).Click();
+            Thread.Sleep(1000);
+        }
+
+        public void pop_up()
+        {
+            try
+            {
+                driver.FindElement(By.ClassName("seg-popup-close")).Click();
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine(ex);
+            }
+        }
+
+        public void kvkk()
+        {
+            try
+            {
+                driver.FindElement(By.CssSelector("div#userKvkkModal span.btnBlack")).Click();
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine(ex);
+            }
+
+            //Console.WriteLine("Sayfa Başlığı" + driver.Title + "sayfa Url:" + driver.Url);
+            //var productPrice = driver.FindElement(By.CssSelector("div#newCheckout table:nth-child(6) div.priceArea span")).Text;
+            //var productPrice2 = driver.FindElement(By.CssSelector("div#newCheckout table:nth-child(5) div.priceArea span")).Text;
+            //Console.WriteLine("1. Ürünün Sepet Fiyatı:" + productPrice + " " + "2. Ürünün Sepet Fiyatı:" + productPrice2);
+        }
+
+        public void scrollDown()
+        {
+            var element = driver.FindElement(By.CssSelector("div#contentListing a.active"));
+            var js = (IJavaScriptExecutor)driver;
+            js.ExecuteScript("arguments[0].scrollIntoView({behavior: 'smooth', block: 'center'})", element);
+        }
+
+        public void productOptions(string x)
+        {
+            driver.FindElement(By.XPath(x)).Click();
+
+            //support kütüphanesi ile option yakalama
+
+            //var element = driver.FindElement(By.Id(x));
+            //var selectElement = new SelectElement(element);
+            //selectElement.SelectByIndex(2);
+            //var element2 = driver.FindElement(By.Id(y));
+            //var selectElement2 = new SelectElement(element2);
+            //selectElement2.SelectByIndex(2);
+
+        }
+
+
+        public void Back()
+        {
+            driver.Navigate().Back();
+        }
+
+        public void login()
+        {
+            driver.FindElement(By.Id("email")).SendKeys("ahmetali.6117@gmail.com");
+            driver.FindElement(By.Id("password")).SendKeys("fnm230596");
+            driver.FindElement(By.Id("loginButton")).Click();
+        }
+    }
+}
---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,81 @@
+﻿using ClassLibrary1.BaseClass;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    [TestFixture]
+    public class Class1 : BaseTest
+    {
+        [Test]
+        public void TestMethod1()
+        {
+            search();
+            //pop_up();
+            scrollDown();
+            cssClick("div#p-449429646 h3.productName");
+            productOptions(".//*[@id = '768877666']/option[2]");
+            productOptions(".//*[@id = '768877667']/option[2]");
+            cssClick("div#contentProDetail a.btnAddBasket");
+            Back();
+            scrollDown();
+            cssClick("div#p-238339646 h3.productName");
+            productOptions(".//*[@id = '754652198']/option[2]");
+            productOptions(".//*[@id = '754652199']/option[2]");
+            cssClick("div#contentProDetail a.btnAddBasket");
+            cssClick("div#wrapper a.myBasket");
+            kvkk();
+            cssClick("span#js-buyBtn");
+            login();
+        }
+
+        [Test]
+        public void TestMethod2()
+        {
+            kategori("//*[@id='contentMain']/div/nav/ul/li[6]/a", "//*[@id='contentMain']/div/nav/ul/li[6]/div/ul/li[2]/a");
+            cssClick("ul#filter-list > li:nth-child(1)");
+            //productOptions(".//*[@id = '772151561']/label[1]");
+            cssClick("#addToBasket");
+            cssClick("div#wrapper a.myBasket");
+            kvkk();
+            cssClick("span#js-buyBtn");
+            login();
+        }
+
+        [Test]
+        public void TestMethod3()
+        {
+            search();
+            scrollDown();
+            cssClick("#brand-klm-Kadın");
+            cssClick("#brand-numara-38");
+            cssClick("#p-390053319 .productName");
+            productOptions(".//*[@id = '743015531']/option[3]");
+            cssClick("div#contentProDetail a.btnAddBasket");
+            cssClick("div#wrapper a.myBasket");
+            kvkk();
+            cssClick("span#js-buyBtn");
+            login();
+        }
+
+        [Test]
+        public void TestMethod4()
+        {
+            cssClick("#contentMain .categoryMenuTab ul > li:nth-child(3)");
+            cssClick("#p-449520953 .productName");
+            cssClick(".addBasketUnify");
+            Back();
+            cssClick("#p-469727351 .productName");
+            cssClick(".addBasketUnify");
+            cssClick("div#wrapper a.myBasket");
+            kvkk();
+            cssClick("span#js-buyBtn");
+            login();
+        }
+    }
+}
